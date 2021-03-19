3a0be8f21e57c016f9687647773772062c5fd7b9
Petr <dat.petr@gmail.com>
2021-03-20 00:34:59 +0300

    1

--- mysite/apps/office_teacher/models.py
+++ mysite/apps/office_teacher/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    test_title = models.CharField('название теста', max_length=200)
+    test_text = models.IntegerField('количество вопросов')
+
+
+class Question(models.Model):
+    question_title = models.TextField('введите вопрос ответа')
+    first_var = models.TextField('введите первый вариант ответа')
+    second_var = models.TextField('введите второй вариант ответа')
+    third_var = models.TextField('введите третий вариант ответа')
+    fourth_var = models.TextField('введите четвёртый вариант ответа')
