6552c759ff3c5949d606b9a613d282c517e7927a
Petr <dat.petr@gmail.com>
2021-03-19 17:58:00 +0300

    1

--- mysite/apps/office_teacher/urls.py
+++ mysite/apps/office_teacher/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'office_teacher'
 
 urlpatterns = [
-    # path('making_test/', views.making_test, name='making_test'),
-    path('making_test', views.making_test, name='making_test'),
+    path('making_test/', views.making_test, name='making_test'),
     path('', views.index, name='index')
 ]
