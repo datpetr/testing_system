ed4bd89647e657a9d662f65ae339f17847a5dd50
Petr <dat.petr@gmail.com>
2021-03-20 08:13:29 +0300

    1

--- mysite/apps/office_teacher/admin.py
+++ mysite/apps/office_teacher/admin.py
@@ -2,4 +2,4 @@ from django.contrib import admin
 from .models import Test, Question
 
 admin.site.register(Test)
-admin.site.register(Question)
+# admin.site.register(Question)
--- mysite/apps/office_teacher/models.py
+++ mysite/apps/office_teacher/models.py
@@ -4,12 +4,6 @@ from django.db import models
 class Test(models.Model):
     test_title = models.CharField('название теста', max_length=200)
     test_text = models.IntegerField('количество вопросов')
-
-    def __str__(self):
-        return self.test_title
-
-
-class Question(models.Model):
     question_title = models.TextField('введите вопрос ответа')
     first_var = models.TextField('введите первый вариант ответа')
     second_var = models.TextField('введите второй вариант ответа')
@@ -17,4 +11,16 @@ class Question(models.Model):
     fourth_var = models.TextField('введите четвёртый вариант ответа')
 
     def __str__(self):
-        return self.question_title
+        return self.test_title
+
+
+class Question(models.Model):
+    # question_title = models.TextField('введите вопрос ответа')
+    # first_var = models.TextField('введите первый вариант ответа')
+    # second_var = models.TextField('введите второй вариант ответа')
+    # third_var = models.TextField('введите третий вариант ответа')
+    # fourth_var = models.TextField('введите четвёртый вариант ответа')
+
+    # def __str__(self):
+        # return self.question_title
+    pass
