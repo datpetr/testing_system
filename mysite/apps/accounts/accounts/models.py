9dfd99bae8fbb47d709ceba577032227769a2836
Petr <dat.petr@gmail.com>
2021-03-20 07:57:36 +0300

    1

--- mysite/apps/accounts/accounts/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class Gmail(models.Model):
-    pass
--- mysite/apps/office_teacher/models.py
+++ mysite/apps/office_teacher/models.py
@@ -5,6 +5,9 @@ class Test(models.Model):
     test_title = models.CharField('название теста', max_length=200)
     test_text = models.IntegerField('количество вопросов')
 
+    def __str__(self):
+        return self.test_title
+
 
 class Question(models.Model):
     question_title = models.TextField('введите вопрос ответа')
@@ -12,3 +15,6 @@ class Question(models.Model):
     second_var = models.TextField('введите второй вариант ответа')
     third_var = models.TextField('введите третий вариант ответа')
     fourth_var = models.TextField('введите четвёртый вариант ответа')
+
+    def __str__(self):
+        return self.question_title
